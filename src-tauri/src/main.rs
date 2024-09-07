e6621c3831ac747186da7573a7bec6c20bd3a139
fuchigta <fuchigta@alpha.co.jp>
2024-09-07 19:37:15 +0900

    画面のラベルをuuidにして複数開けるようにした

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,45 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use uuid::Uuid;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn save_sticker(window: tauri::Window, markdown: &str) {
+    print!("{0} {1}", window.label(), markdown)
+}
+
+#[tauri::command]
+fn new_sticker(handle: tauri::AppHandle) {
+    let label = Uuid::new_v4();
+
+    let w = tauri::WindowBuilder::new(
+        &handle,
+        label,
+        tauri::WindowUrl::App("index.html".into()),
+    )
+    .build()
+    .unwrap();
+
+    w.set_title("mdsticker");
+}
+
+#[tauri::command]
+fn remove_sticker(window: tauri::Window) {
+    print!("{0} removed", window.label());
+    window.close();
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            save_sticker,
+            new_sticker,
+            remove_sticker
+        ])
+        .setup(|app| {
+            new_sticker(app.handle());
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { Fragment, useState } from "react";
 import "./App.css";
 import Markdown from "react-markdown";
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
-import {solarizedDarkAtom} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { solarizedDarkAtom } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { invoke } from "@tauri-apps/api";
+
+const sticker = {
+  async create() {
+    await invoke("new_sticker");
+  },
+  async save(markdown: string) {
+    await invoke("save_sticker", { markdown });
+  },
+  async remove() {
+    await invoke("remove_sticker");
+  },
+};
 
 function App() {
   const [editting, setEditting] = useState(false);
@@ -11,14 +24,35 @@ function App() {
   return (
     <div className="container">
       <header>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            setEditting(!editting);
-          }}
-        >
-          {editting ? "DONE" : "EDIT"}
-        </button>
+        <div className="controller">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setEditting(!editting);
+              sticker.save(markdown);
+            }}
+          >
+            {editting ? "DONE" : "EDIT"}
+          </button>
+        </div>
+        <div className="manager">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              sticker.create();
+            }}
+          >
+            NEW
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              sticker.remove();
+            }}
+          >
+            REMOVE
+          </button>
+        </div>
       </header>
       <main>
         <Fragment>
@@ -33,27 +67,29 @@ function App() {
           ) : (
             <div className="markdown">
               <Markdown
-              children={markdown}
-              components={{
-                code(props) {
-                  const {children, className, node, ref, ...rest} = props
-                  const match = /language-(\w+)/.exec(className || '')
-                  return match ? (
-                    <SyntaxHighlighter
-                      ref={ref as React.LegacyRef<SyntaxHighlighter> | undefined}
-                      {...rest}
-                      PreTag="div"
-                      children={String(children).replace(/\n$/, '')}
-                      language={match[1]}
-                      style={solarizedDarkAtom}
-                    />
-                  ) : (
-                    <code ref={ref} {...rest} className={className}>
-                      {children}
-                    </code>
-                  )
-                }
-              }}
+                children={markdown}
+                components={{
+                  code(props) {
+                    const { children, className, node, ref, ...rest } = props;
+                    const match = /language-(\w+)/.exec(className || "");
+                    return match ? (
+                      <SyntaxHighlighter
+                        ref={
+                          ref as React.LegacyRef<SyntaxHighlighter> | undefined
+                        }
+                        {...rest}
+                        PreTag="div"
+                        children={String(children).replace(/\n$/, "")}
+                        language={match[1]}
+                        style={solarizedDarkAtom}
+                      />
+                    ) : (
+                      <code ref={ref} {...rest} className={className}>
+                        {children}
+                      </code>
+                    );
+                  },
+                }}
               />
             </div>
           )}
